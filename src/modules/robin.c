f2679aa4ab23f5dbdfaf10c99fe554048500e2f5
suncaishun <suncaishun@ypshengxian.com>
2021-03-30 20:47:27 +0800

    -add snowflake.so

--- src/modules/robin.c
+++ src/modules/robin.c
@@ -13,7 +13,7 @@ int ping_command(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     REDISMODULE_NOT_USED(argc);
 
     RedisModuleString *s = RedisModule_CreateString(ctx,"are you ok ?",12);
-    //RedisModule_StringAppendBuffer(ctx,s,"bar",3);
+    RedisModule_StringAppendBuffer(ctx,s," yeah",5);
     RedisModule_ReplyWithString(ctx,s);
     RedisModule_FreeString(ctx,s);
     return REDISMODULE_OK;
